50a5b2e3a671a95b6c47a625810af2a2ec155889
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2025-01-28 09:49:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String S = br.readLine().trim();
+            Solution obj = new Solution();
+            ArrayList<String> ans = obj.findPermutation(S);
+            Collections.sort(ans);
+            for (int i = 0; i < ans.size(); i++) {
+                out.print(ans.get(i) + " ");
+            }
+            out.println();
+
+            out.println("~");
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    public void solve(StringBuilder sb,HashSet<String> hs,ArrayList<String> al,int i,int n){
+        if(i >= n) return;
+        if(i == n - 1){
+            if(!hs.contains(sb.toString())){
+                al.add(sb.toString());
+                hs.add(sb.toString());
+            }
+        }
+        for(int j = i ; j < n ; j++){
+            swap(sb,i,j);
+            solve(sb,hs,al,i+1,n);
+            swap(sb,i,j);
+        }
+    }
+    public ArrayList<String> findPermutation(String s) {
+        // Code here
+        StringBuilder sb = new StringBuilder(s);
+        HashSet<String> hs = new HashSet<>();
+        ArrayList<String> al = new ArrayList<>();
+        int n = sb.length();
+        solve(sb,hs,al,0,n);
+        // Collections.sort(al);
+        return al;
+    }
+    public void swap(StringBuilder sb,int i,int j){
+        char temp = sb.charAt(i);
+        sb.setCharAt(i,sb.charAt(j));
+        sb.setCharAt(j,temp);
+    }
+}
