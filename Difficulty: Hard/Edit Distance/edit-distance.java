85f4e1dcba189ecb2996a2180e5ee55fa31d70ec
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2025-03-10 20:31:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.java
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -9,12 +9,13 @@ class GFG {
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String s1 = br.readLine();
-            String[] S = s1.split(" ");
-            String s = S[0];
-            String t = S[1];
+            String s2 = br.readLine();
+
             Solution ob = new Solution();
-            int ans = ob.editDistance(s, t);
+            int ans = ob.editDistance(s1, s2);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -22,38 +23,36 @@ class GFG {
 // } Driver Code Ends
 
 
+
 class Solution {
-    int[][] dp = new int[101][101];
-    public int solve(String str1,String str2,int i,int j)
-    {
-        if(i == str1.length())
-        {
-            return str2.length() - j;
-        }
-        if(j == str2.length())
-        {
-            return str1.length() - i;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE, c = Integer.MAX_VALUE, d = Integer.MAX_VALUE;
-        if(str1.charAt(i) == str2.charAt(j))
-        {
-            return dp[i][j] = solve(str1,str2,i+1,j+1);
-        }
-        else
-        {
-            b = 1 + solve(str1,str2,i+1,j);
-            c = 1 + solve(str1,str2,i+1,j+1);
-            d = 1 + solve(str1,str2,i,j+1);
-        }
-        return dp[i][j] = Math.min(b,Math.min(c,d));
-    }
-    public int editDistance(String str1, String str2) {
+    public int editDistance(String s1, String s2) {
         // Code here
-        for(int[] dp1 : dp){
-            Arrays.fill(dp1,-1);
-        }
-        int ans = solve(str1,str2,0,0);
-        return ans;
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        
+         for(int i=0;i<=n;i++)
+         {
+          dp[i][0] = i;
+         }
+         for(int j=0;j<=m;j++)
+         {
+            dp[0][j] = j;
+         }
+         for(int i=1;i<=n;i++)
+         {
+             for(int j=1;j<=m;j++)
+             {
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                {
+                     dp[i][j] = dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1])); 
+                }
+             }
+         }
+        return dp[n][m];
     }
 }
