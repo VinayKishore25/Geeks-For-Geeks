a13eb99555210477b30ce8f9cec70e8304a4b7a1
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2025-03-13 18:52:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input (arr)
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] =
+                    Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input (dep)
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] dep = new int[m];
+            for (int i = 0; i < m; i++) {
+                dep[i] =
+                    Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution obj = new Solution();
+            System.out.println(obj.findPlatform(arr, dep));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[]) {
+        // add your code here
+        PriorityQueue<int[]> platforms = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        int n = dep.length;
+        int minPlatforms = 1;
+        for(int i = 0 ; i < n ; i++){
+            platforms.add(new int[]{arr[i],dep[i]});
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] platform = platforms.poll();
+        pq.add(platform[1]);
+        while(!platforms.isEmpty()){
+            platform = platforms.poll();
+            // System.out.println(Arrays.toString(platform));
+            if(!pq.isEmpty()){
+                int currDept = pq.poll();
+                if(platform[0] > currDept){
+                    pq.add(platform[1]);
+                }
+                else{
+                    pq.add(currDept);
+                    pq.add(platform[1]);
+                }
+                minPlatforms = Math.max(minPlatforms,pq.size());
+            }
+        }
+        return minPlatforms;
+    }
+}
