1a7b120819eed197fd2a8753c138e725e5f5eba4
MUNJULURI V V D SURYA KISHORE VINAY <vinaykishore2512@gmail.com>
2026-01-12 20:44:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -1,75 +1,31 @@
-//{ Driver Code Starts
-// Initial template for JAVA
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        // taking input using class Scanner
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            // taking total number of elements
-            int k = Integer.parseInt(br.readLine());
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-            ArrayList<Integer> res = new Solution().max_of_subarrays(k, arr);
-
-            // printing the elements of the ArrayList
-            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
-            System.out.println();
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function template for JAVA
-
 class Solution {
-    // Function to find maximum of each subarray of size k.
-    public ArrayList<Integer> max_of_subarrays(int k, int arr[]) {
-        // Your code here
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        
-        int tempMax= findMax(0,k-1,arr);
-        al.add(tempMax);
-        
-        for(int i=1; i<=arr.length-k; i++){
-            if(tempMax == arr[i-1] ){
-                tempMax = findMax(i, i+k-1,arr);
-                al.add(tempMax);
-            }
-            else{
-                tempMax = Math.max(tempMax, arr[i+k-1]);
-                al.add(tempMax);
-            }
-        }
-        return al;
-        
-    }
-    
-    public int findMax(int start, int end, int arr[]){
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        // code here
         int max=0;
-        while(start <= end){
-            max = Math.max(max, arr[start]);
-            start++;
+        ArrayList<Integer> list=new ArrayList<>();   
+        for(int i=0;i<k;i++)
+        {
+            if(max<arr[i])
+                max=arr[i];
         }
-        return max;
+        list.add(max);
+        int len=k;
+        for(int i=0;i<arr.length-len;i++)
+        {
+            if(arr[i]==max)
+            {
+                max=0;
+                for(int j=i+1;j<=k;j++)
+                {
+                    if(max<arr[j])
+                        max=arr[j];
+                }
+            }
+            if(arr[k]>max)
+                max=arr[k];
+            list.add(max);
+            k++;
+        }
+        return list;
     }
 }
