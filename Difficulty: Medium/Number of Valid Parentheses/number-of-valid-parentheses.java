1ab39c433d931717d31ccc3357abec3d82de2c05
MUNJULURI V V D SURYA KISHORE VINAY <vinaykishore2512@gmail.com>
2026-01-25 17:56:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Valid Parentheses/number-of-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    int findWays(int n) {
+        // code here
+        if(n%2!=0){
+            return 0;
+        }
+        int k=n/2;
+        int []dp=new int[k+1];
+        dp[0]=1;
+        
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i]+=dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[k];
+    }
+}
