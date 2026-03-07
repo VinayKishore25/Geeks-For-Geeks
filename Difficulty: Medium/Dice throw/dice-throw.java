188b33808df280c1df68b35c127aeb6db7506f51
MUNJULURI V V D SURYA KISHORE VINAY <vinaykishore2512@gmail.com>
2026-03-07 23:07:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dice throw/dice-throw.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int noOfWays(int m, int n, int x) {
+         int[][] dp = new int[n+1][x+1];
+
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= x; j++) {
+                dp[i][j] = 0;
+                for (int f = 1; f <= m; f++) {
+                    if (j - f >= 0) {
+                        dp[i][j] += dp[i-1][j-f];
+                    }
+                }
+            }
+        }
+
+        return dp[n][x];
+    }
+};
